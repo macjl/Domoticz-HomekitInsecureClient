b74d17a39bb329e3d2b80026d5976410df04ff0a
MacJL <jeanlaurent.girod@icloud.com>
2021-04-26 19:10:27 +0200

    Handle multiple Characteristics by accessories

--- plugin.py
+++ plugin.py
@@ -11,11 +11,18 @@
         <param field="Address" label="IP Address of homekit device or bridge" width="200px" required="true" default="127.0.0.1"/>
         <param field="Port" label="PORT of homekit device or bridge" width="50px" required="true" default="54821"/>
         <param field="Password" label="Authorization key" width="80px" required="true" default="031-45-154"/>
+        <param field="Mode6" label="Debug" width="150px">
+            <options>
+                <option label="None" value="0"  default="true" />
+                <option label="Plugin Debug" value="2"/>
+                <option label="All" value="-1"/>      
+            </options>
+        </param>
     </params>
 </plugin>
 """
 import Domoticz
-import json
+import json, time
 
 class BasePlugin:
     enabled = False
@@ -26,60 +33,79 @@ class BasePlugin:
         return
 
     def onStart(self):
-        Domoticz.Log("onStart called")
-        Domoticz.Debugging(0)
+        Domoticz.Debugging(int(Parameters["Mode6"]))
+        Domoticz.Debug("Plugin set in Debug Mode")
         DumpConfigToLog()
-        self.headers = { 'Content-Type': 'Application/json',
-                'Authorization': Parameters["Password"]}
-        self.iid = Parameters["Mode2"]
+
+        Domoticz.Log("Initializing Homekit Insecure Client Plugin")
+
+        Domoticz.Debug("Creating Connection object")
+        self.headers = { 'Content-Type': 'Application/json', 'Authorization': Parameters["Password"] }
         self.httpConn = Domoticz.Connection(Name="Homekit", Transport="TCP/IP", Protocol="HTTP", Address=Parameters["Address"], Port=Parameters["Port"])
+
+        Domoticz.Log("Connecting to Homekit Device at address http://" + Parameters["Address"] + ":" + Parameters["Port"] )
         self.httpConn.Connect()
 
     def onStop(self):
         Domoticz.Log("onStop called")
 
     def onConnect(self, Connection, Status, Description):
-        Domoticz.Log("onConnect called")
         if (Status == 0):
-            Domoticz.Debug("Connection successfull")
-            self.httpConn.Send({'Verb':'GET', 'URL':'/accessories', 'Headers': self.headers})
+            Domoticz.Log("Connection successfull")
         else:
-            Domoticz.Log("Failed to connect ("+str(Status)+") to: "+Parameters["Address"]+":"+Parameters["Port"]+" with error: "+Description)
+            Domoticz.Error("Failed to connect ("+str(Status)+") to: "+Parameters["Address"]+":"+Parameters["Port"]+" with error: "+Description)
 
     def onMessage(self, Connection, Data):
-        Domoticz.Log("onMessage called")
-        DumpHTTPResponseToLog(Data)
-        strData = Data["Data"].decode("utf-8", "ignore")
-        accessories = json.loads(strData)["accessories"]
+        Domoticz.Debug("Data received")
+
+        Status = int(Data["Status"])
+
+        if (Status != 200 and Status != 204):
+            Domoticz.Error("Invalid Data received. Status=" + str( Status) )
+            return
+        elif (Status == 204):
+            Domoticz.Debug( "Command sent")
+            return
+
+        # Get Accessories as a dict variable
+        accessories = json.loads( Data["Data"].decode("utf-8", "ignore") )["accessories"]
         for accessory in accessories:
             hkaid = accessory["aid"]
             supported = 0
+            # Find if accessory is provided by eDomoticz
             for service in accessory["services"]:
                 if( service["type"] == "3E" ):
                     for characteristic in service["characteristics"]:
-                        if ( characteristic["type"] == "23" ):
-                            hkName = characteristic["value"]
                         if ( characteristic["type"] == "20" ):
                             hkManufacturer = characteristic["value"]
-                if( service["type"] == "47" ):
-                    for characteristic in service["characteristics"]:
-                        if ( characteristic["type"] == "25" ):
-                            hkiid = str( characteristic["iid"] )
-                            hkValue = characteristic["value"]
-                            supported = 1
-            if ( hkManufacturer == "eDomoticz" ):
-                supported = 0
-            if ( supported == 1 ):
-                Domoticz.Log( hkManufacturer + " - " + hkName + " - " + str( hkaid ) + " - " + hkiid + " - " + str (hkValue) )
-                try:
-                    Devices[hkaid].Touch
-                except:
-                    Domoticz.Device(Name=hkName, Unit=hkaid, TypeName="Switch", DeviceID=hkiid).Create()
-                    Domoticz.Log("Device created.")
-                if ( hkValue == 1 ):
-                    Devices[hkaid].Update(nValue=1,sValue="On")
-                if ( hkValue == 0 ):
-                    Devices[hkaid].Update(nValue=0,sValue="Off")
+            # If accessory is not provided by eDomoticz
+            if ( hkManufacturer != "eDomoticz" ):
+                for service in accessory["services"]:
+                    # Service of type Smart Plug
+                    if( service["type"] == "47" or service["type"] == "49" ):
+                        for characteristic in service["characteristics"]:
+                            if ( characteristic["type"] == "23" ):
+                                hkName = characteristic["value"]
+                            if ( characteristic["type"] == "25" ):
+                                hkiid = characteristic["iid"]
+                                hkValue = characteristic["value"]
+                        deviceID = service["type"] + "-" + str( hkaid ) + "-" + str( hkiid )
+                        domoticzID = GetIDFromDevID( deviceID )
+                        Domoticz.Log( hkManufacturer + " : " + hkName + " - DeviceID=" + deviceID + " - DomoticzID=" + str( domoticzID ) + " - Current Value=" + str (hkValue) )
+
+                        if ( domoticzID == -1 ):
+                            Domoticz.Debug("Create domoticz device :\"" + hkName + "\" with ID=" + str( len(Devices) + 1 ) + " and DeviceID=" + deviceID + " of type Switch")
+                            Domoticz.Device(Name=hkName, Unit=len(Devices) + 1, TypeName="Switch", DeviceID=deviceID ).Create()
+                            domoticzID = GetIDFromDevID( deviceID )
+                            Domoticz.Log("Device created.")
+                        if ( hkValue == 1 ):
+                            Devices[domoticzID].Update(nValue=1,sValue="On")
+                        if ( hkValue == 0 ):
+                            Devices[domoticzID].Update(nValue=0,sValue="Off")
+                    elif( service["type"] == "3E"):
+                        pass
+                    else:
+                        Domoticz.Debug("Device " + hkManufacturer + " - AID=" + str( hkaid ) + " - Type of Service=" + service["type"] + " - Not supported yet")
 
     def onCommand(self, Unit, Command, Level, Hue):
         if ( Devices[Unit].sValue != Command ):
@@ -90,29 +116,32 @@ class BasePlugin:
                 Devices[Unit].Update(nValue=0,sValue="Off")
                 nValue = "0"
 
-            aid = str( Unit )
-            iid = str( Devices[Unit].DeviceID )
+            deviceIDsplitted = Devices[Unit].DeviceID.split("-")
+            aid = deviceIDsplitted[1]
+            iid = deviceIDsplitted[2]
 
-            Domoticz.Log("Command called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "'")
+            Domoticz.Log("Command called for Unit=" + str(Unit) + " and DeviceID=" + Devices[Unit].DeviceID + ": Parameter '" + str(Command) + "'")
             data = "{\"characteristics\":[{\"aid\":" + aid + ",\"iid\":" + iid + ",\"value\":" + nValue + "}]}"
-            #Domoticz.Log(data)
+            Domoticz.Debug(data)
             self.httpConn.Send({'Verb':'PUT', 'URL':'/characteristics', 'Headers': self.headers, 'Data': data})
+            onHeartbeat()
 
     def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
         Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
 
     def onDisconnect(self, Connection):
-        Domoticz.Log("onDisconnect called")
+        Domoticz.Log("Reconnecting")
+        time.sleep(10)
+        self.httpConn.Connect()
 
     def onHeartbeat(self):
-        Domoticz.Log("onHeartbeat called")
-        if (self.httpConn != None and (self.httpConn.Connecting() or self.httpConn.Connected())):
+        Domoticz.Debug("Refreshing Accessories Status")
+        if (self.httpConn != None and ( self.httpConn.Connected() )):
             Domoticz.Debug("Connection is alive.")
+            self.httpConn.Send({'Verb':'GET', 'URL':'/accessories', 'Headers': self.headers})
         else:
-            Domoticz.Debug("Connection Lost. Reconnecting.")
-            self.httpConn.Connect()
-
-        self.httpConn.Send({'Verb':'GET', 'URL':'/accessories', 'Headers': self.headers})
+            Domoticz.Error("Connection Lost. Reconnecting.")
+            self.httpConn.Disconnect()
 
 global _plugin
 _plugin = BasePlugin()
@@ -130,7 +159,6 @@ def onConnect(Connection, Status, Description):
     _plugin.onConnect(Connection, Status, Description)
 
 def onMessage(Connection, Data):
-    DumpHTTPResponseToLog(Data)
     global _plugin
     _plugin.onMessage(Connection, Data)
 
@@ -151,6 +179,13 @@ def onHeartbeat():
     _plugin.onHeartbeat()
 
     # Generic helper functions
+
+def GetIDFromDevID(devid):
+    for Device in Devices:
+        if ( devid == Devices[Device].DeviceID ):
+            return Device
+    return -1
+
 def DumpConfigToLog():
     for x in Parameters:
         if Parameters[x] != "":
